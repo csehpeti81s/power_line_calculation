8a6ee62b0925ab2bbc9e05c4225a93ee3b13f68e
Péter Cseh <peter.cseh@outlook.com>
2025-11-22 09:22:01 +0100

    202511220921

--- eloszlasszamito.py
+++ eloszlasszamito.py
@@ -36,7 +36,37 @@ def quadraticFormula(a, b, c):
     x2=(-b - cmath.sqrt(D))/(2*a)
     return (x1, x2)
 
+class oszlopkoz():
+    def __init__(self):
+        self.Dpoles = 0.300     #lengt in kilometers between this and previous poles
+        self.Rpolegr = 5        #earth resistance of the current pole in Ω
+        self.Rsubstgr = 0.05    #earth resistance of the substation in Ω
+        self.Astw = 95 + 50     #cross section of the static wire in mm2
+        self.Aphw = 250 + 40    #cross section of the phase wire in mm2
+        self.Rphw = 0.1154      #resistivity of the phase wire in Ω
+        self.Rstw = 0.2992      #resistivity of the static wire in Ω
+        self.Dphstw = 10        #distance of ground and static wire in meter
+        self.Zstw = 0           #self impedance of static wire ground loop
+        self.Zphw =0            #sefl ipmedance of phase wire ground loop
+        self.Zphstmut = 0       #mutual impedance of phase and static to ground loops 
+        self.calculateInternalValues()
+    def calculateInternalValues(self):
+        self.Zstw = self.Dpoles * complex(self.Rstw, selfReactance(1000, GMRfromA(self.Astw)))  
+        self.Zphw = self.Dpoles * complex(self.Rphw, selfReactance(1000, GMRfromA(self.Aphw)))  
+        self.Zphstmut = self.Dpoles * complex(0, mutReactance(1000, self.Dphstw))
+    def calculataBackward(self, next):
+        pass
+    def calculateForward(self, previous):
+        pass
+
 def main():
+    polesCount = 5     #Number of poles in the line included the faulty one. 
+    ok = oszlopkoz()
+    oktxt = ( f'Táv: {ok.Dpoles*1000:.0f} m, Zgrw: {ok.Zstw:5.2f} Ω, Zphw: {ok.Zphw:5.2f} Ω, ' 
+        f'Zphstmut: {ok.Zphstmut:2.0f} Ω')
+    print(oktxt)
+
+def tst():
     I_zref = complex(10,0)
     oszlopkozokSzama = 40
     l_ok = 0.3                                                 #Oszlopköz km-ben
@@ -74,6 +104,7 @@ def main():
                 U_e = U_e * R_sz / (R_sz + Z_g) + U_fg 
                 I_sz = abs(U_e / R_sz)
                 print(datatxt)
+
 
 if __name__=="__main__":
     main()
