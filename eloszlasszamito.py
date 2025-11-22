89ffe9f34a5246bf8e25939a4b8dc60798108600
Péter Cseh <peter.cseh@outlook.com>
2025-11-22 08:23:01 +0100

    20251121

--- eloszlasszamito.py
+++ eloszlasszamito.py
@@ -2,7 +2,7 @@
 import math, cmath, numpy
 
 DEBUG=True
-DEBUG=False
+#DEBUG=False
 
 def replusZ(Z1, Z2):
     return (Z1*Z2)/(Z1+Z2)
@@ -44,30 +44,36 @@ def main():
     A_f = 250 + 40
     D_fg = 10                                               #m-ben megadva
     R_sz = complex(5,0)
-    R_szall = complex(0,0)
+    R_szall = complex(0.00,0)
     Z_g = l_ok * complex(0.2992,selfReactance(1000, GMRfromA(A_g)))
     Z_f = l_ok * complex(0.1154,selfReactance(1000, GMRfromA(A_f)))
-    Z_fg = complex(0, l_ok * mutReactance(1000, D_fg))
+    Z_fg = complex(0, l_ok * mutReactance(1000, D_fg)) 
+    U_fg = I_zref * Z_fg
+
+    if(DEBUG):
+        debhead= (f'{"":*^80}\n{"Zg":>16}{"Zf":>16}{"Zfg":>16}{"Ufg":>16}\n')
+        debtxt = (f'{Z_g:16.3f}{Z_f:16.3f}{Z_fg:16.3f}{U_fg:16.3f}\n{"":*^80}')
+        print(debhead,debtxt)
 
     if(True):
         for nOszlopkoz in range(oszlopkozokSzama):
             pass
         if(True): 
-            nOszlopkoz = 9
+            nOszlopkoz = 40
             nOszlopkoz = nOszlopkoz 
-            headertxt = (f'{"n":>3}{"N":>4}{"Ze":>16}{"Ig/Iz":>6}{"Ue":>6}{"Isz":>6}')
+            headertxt = (f'{"n":>3}{"N":>4}{"Ze":>16}{"Ue":>16}{"Ig/Iz\'":>7}{"Isz":>6}')
             print(headertxt)
             Z_e = R_szall + Z_g
-            U_e = U_fg = I_zref * Z_fg
+            U_e = U_fg
             I_sz = abs(U_e / R_sz)
-            for i in range(nOszlopkoz + 1):
+            for i in range(nOszlopkoz):
                 Ig_ratio, void = currentRatio(Z_e, R_sz)
-                txt = ( f'{nOszlopkoz-i:3d} {nOszlopkoz:3d}{Z_e:>16.2f}{abs(Ig_ratio):6.2f}'
-                    f'{abs(U_e):>6.2f}{abs(I_sz):6.2f}')
+                datatxt = ( f'{nOszlopkoz-i-1:3d} {nOszlopkoz-1:3d}{Z_e:>16.2f}'
+                    f'{U_e:>16.2f}{abs(Ig_ratio):7.2f}{abs(I_sz):6.2f}')
                 Z_e = replusZ(Z_e, R_sz) + Z_g
                 U_e = U_e * R_sz / (R_sz + Z_g) + U_fg 
                 I_sz = abs(U_e / R_sz)
-                print(txt)
+                print(datatxt)
 
 if __name__=="__main__":
     main()
